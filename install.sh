fc0e32271a893627c26542778fa35661517a2441
Xian Guo <iamxguo@aol.com>
2018-01-21 11:41:02 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+# -*- Mode:bash; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+#
+# Copyright (C) 2015-2017, The University of Memphis,
+#                          Arizona Board of Regents,
+#                          Regents of the University of California.
+#
+# This file is part of Mini-NDN.
+# See AUTHORS.md for a complete list of Mini-NDN authors and contributors.
+#
+# Mini-NDN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mini-NDN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mini-NDN, e.g., in COPYING.md file.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# This file incorporates work covered by the following copyright and
+# permission notice:
+#
+#   Mininet 2.3.0d1 License
+#
+#   Copyright (c) 2013-2016 Open Networking Laboratory
+#   Copyright (c) 2009-2012 Bob Lantz and The Board of Trustees of
+#   The Leland Stanford Junior University
+#
+#   Original authors: Bob Lantz and Brandon Heller
+#
+#   We are making Mininet available for public use and benefit with the
+#   expectation that others will use, modify and enhance the Software and
+#   contribute those enhancements back to the community. However, since we
+#   would like to make the Software available for broadest use, with as few
+#   restrictions as possible permission is hereby granted, free of charge, to
+#   any person obtaining a copy of this Software to deal in the Software
+#   under the copyrights without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included
+#   in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#   The name and trademarks of copyright holder(s) may NOT be used in
+#   advertising or publicity pertaining to the Software or any derivatives
+#   without specific, written prior permission.
+
+test -e /etc/debian_version && DIST="Debian"
+grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
+
+if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    update='sudo apt-get update'
+    install='sudo apt-get -y install'
+    remove='sudo apt-get -y remove'
+    pkginst='sudo dpkg -i'
+    # Prereqs for this script
+    if ! which lsb_release &> /dev/null; then
+        $install lsb-release
+    fi
+fi
+
+test -e /etc/fedora-release && DIST="Fedora"
+if [[ $DIST == Fedora ]]; then
+    update='sudo yum update'
+    install='sudo yum -y install'
+    remove='sudo yum -y erase'
+    pkginst='sudo rpm -ivh'
+    # Prereqs for this script
+    if ! which lsb_release &> /dev/null; then
+        $install redhat-lsb-core
+    fi
+fi
+
+
+
+
+# mininetwifi function for install mininetwifi moudul
+function mininetwifi {
+    if [[ updated != true ]]; then
+        $update
+        updated="true"
+    fi
+
+    if [[ $pysetup != true ]]; then
+        pysetup="true"
+    fi
+
+    #git clone --depth 1 https://github.com/intrig-unicamp/mininet-wifi
+    cd mininet-wifi
+    sudo ./util/install.sh -Wnfvl
+    cd ../
+}
+
+
+function minindn {
+    if [[ updated != true ]]; then
+        if [ ! -d "build" ]; then
+            $update
+            updated="true"
+        fi
+    fi
+
+    if [[ $pysetup != true ]]; then
+        $install python-setuptools
+        pysetup="true"
+    fi
+    git clone --depth 1 https://github.com/named-data/mini-ndn
+    sudo mv mini-ndn mini_ndn # name mini-ndn is not allowed in python 
+    cd mini_ndn
+    sudo cp ../ndnwifi/experiments/__init__.py ./ #inorder to import module in the subdirecorty mini_ndn
+    sudo ./install.sh -a
+    cd ../
+}
+
+function minindnwifi {
+    if [[ updated != true ]]; then
+        $update
+        updated="true"
+    fi
+
+    if [[ $pysetup != true ]]; then
+        pysetup="true"
+    fi
+    install_dir="/usr/local/etc/mini-ndn/wifi/"
+    sudo mkdir -p "$install_dir"
+    sudo cp ndnwifi_utils/topologies/adhoc-topology.conf "$install_dir"
+    sudo cp ndnwifi_utils/topologies/singleap-topology.conf "$install_dir"
+    sudo cp ndnwifi_utils/topologies/multiap-topology.conf "$install_dir"
+    sudo cp ndnwifi_utils/topologies/vndn-topology.conf "$install_dir"
+    sudo python setup.py clean --all install
+}
+
+
+function usage {
+    printf '\nUsage: %s [-miw]\n\n' $(basename $0) >&2
+
+    printf 'options:\n' >&2
+    printf -- ' -m: install mininet-wifi and dependencies\n' >&2
+    printf -- ' -i: install mini-ndn\n' >&2
+    printf -- ' -w: install minindn-wifi' >&2
+    exit 2
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+else
+    while getopts 'miw' OPTION
+    do
+        case $OPTION in
+        m)    mininetwifi;;
+        i)    minindn;;
+        w)    minindnwifi;;
+        ?)    usage;;
+        esac
+    done
+    shift $(($OPTIND - 1))
+fi
