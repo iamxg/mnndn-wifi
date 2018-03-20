cd51de7c84c11ecb22d16033188a79881b34db8d
Xian Guo <iamxguo@aol.com>
2018-03-20 08:25:18 -0500

    Add files via upload

---
+++ mem-data/lx.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+def main():
+    N = 30
+    fig=plt.figure()
+    ax=fig.add_subplot(111)   
+    plt.axis('scaled')
+    ax.set_xlim([ 0, N])
+    ax.set_ylim([-0.5, 0.5])
+    cmap = get_cmap(N)
+    for i in range(N):
+        rect = plt.Rectangle((i, -0.5), 1, 1, facecolor=cmap(i))
+        ax.add_artist(rect)
+    ax.set_yticks([])
+    plt.show()
+
+if __name__=='__main__':
+    main()
+
+
